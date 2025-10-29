94da7934c0eae9f7036a8ac55067b422f313e4d5
LukeZHar <luke.z.harris@hotmail.com>
2025-10-29 21:28:49 +1000

    Feat: Set up db link and call

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
 
