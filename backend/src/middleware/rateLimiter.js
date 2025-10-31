da2f964d9e72f476f388d79a05ea31a745668be2
LukeZHar <luke.z.harris@hotmail.com>
2025-10-31 13:24:20 +1000

    Feat: Implemented rate limitier with Redis

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,16 @@
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await rateLimiter.limit("my-limit-key");
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "Too many requests, please try again later." });
+    }
+    next();
+  } catch (error) {
+    console.error("Rate limiter error:", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,29 @@
-import express from 'express';
-import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
 // Middleware
 app.use(express.json());
+app.use(rateLimiter);
+
+// Simple request logger middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api/notes", notesRoutes);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+// Connect to the DB then Start the server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
 });
-
