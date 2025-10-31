10b197c97cbd7dafb72c538f7d928143e68d82e3
LukeZHar <luke.z.harris@hotmail.com>
2025-10-31 23:39:26 +1000

    feat: add CORS support, implement rate limiting UI, and enhance HomePage with note fetching

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,8 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
+
 
 // Simple request logger middleware
 app.use((req, res, next) => {
